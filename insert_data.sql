940e399fbd8c8a350564d4e2b15cf1fcffbe329e
dhawan9 <54247789+dhawan9@users.noreply.github.com>
2022-05-02 00:05:18 -0400

    Add files via upload

---
+++ insert_data.sql
@@ -0,0 +1,127 @@
+/*
+Insert into locations(location_description,Location_Address,Location_City,Location_State,Location_Zip)
+values('Folletts Book Store','1400 W State St','West Lafayette','IN','47906');
+
+Insert into locations(location_description, Location_Address,Location_City,Location_State,Location_Zip)
+values('Four points by Sheraton','1600 Cumberland Ave','West Lafayette','IN','47906');
+
+Insert into locations(location_description,Location_Address,Location_City,Location_State,Location_Zip)
+values('Courtyard Marriot','150 Fairington Ave','Lafayette','IN','47905');
+
+Insert into locations(location_description,Location_Address,Location_City,Location_State,Location_Zip)
+values('Double Tree by Hilton','155 Progress Dr','Lafayette','IN','47905');
+
+Insert into locations(location_description,Location_Address,Location_City,Location_State,Location_Zip)
+values('JW Marriott','10 S West St,','Indianapolis','IN','46204');
+
+Insert into locations(location_description,Location_Address,Location_City,Location_State,Location_Zip)
+values('Indianapolis Airport','7800 Col.H.Weir Cook Memorial Dr,','Indianapolis','IN','46241');
+
+*/
+
+
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Dodge','Durango',2020,'Blue','1456722300',1,1,12);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Dodge','Durango',2021,'Black','245672222300',2,1,12);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Dodge','Durango',2020,'white','145992222300',3,1,12);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Dodge','Durango',2020,'Grey','995672222300',4,1,12);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Dodge','Durango',1999,'Blue','18872222003',5,1,9);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Dodge','Durango',1995,'Blue','177672222300',6,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Dodge','Durango',2020,'Green','1995672222300',7,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Dodge','Durango',1995,'Blue','143423222003',8,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Dodge','Durango',2020,'Black','148872222300',9,1,12);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Dodge','Durango',2020,'Blue','145622222300',10,1,12);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Toyota','Sienna',2020,'Blue','1456722300',1,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Toyota','Sienna',2021,'Black','245672222300',2,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Toyota','Sienna',2020,'white','145992222300',3,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Toyota','Sienna',2020,'Grey','995672222300',4,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Toyota','Sienna',1999,'Blue','18872222003',5,1,9);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Toyota','Sienna',1995,'Blue','177672222300',6,1,11);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Toyota','Sienna',2020,'Green','1995672222300',7,1,11);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Toyota','Sienna',1995,'Blue','143423222003',8,1,9);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Toyota','Sienna',2020,'Black','148872222300',9,1,11);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Toyota','Sienna',2020,'Blue','145622222300',10,1,12);
+
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Toyota','Civic',2019,'White','113456',2,1,7);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (2,'Honda','Pilot',2019,'Black','1453323',3,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (3,'Honda','Odyssey',2018,'Grey','2458823',null,0,7);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Honda','Odyssey',2020,'Blue','24777823',null,0,7);
+
+
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Nissan','Altima',2020,'Blue','14569922300',1,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Nissan','Altima',2021,'Black','245699222300',2,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Nissan','Altima',2020,'white','145999222300',3,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Nissan','Altima',2020,'Grey','995699222300',4,1,10);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Nissan','Altima',1999,'Blue','18879922003',5,1,9);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Nissan','Altima',1995,'Blue','177679922300',6,1,11);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Nissan','Altima',2020,'Green','1995679922300',7,1,11);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Nissan','Altima',1995,'Blue','143429922003',8,1,9);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Nissan','Altima',2020,'Black','148899222300',9,1,11);
+
+Insert into cars (Car_type_id ,Make,Model,Make_Year,Color,VIN,Current_location_id,Available_for_rent,Rental_price_per_hour)
+values (1,'Nissan','Altima',2020,'Blue','145622992300',10,1,12);
