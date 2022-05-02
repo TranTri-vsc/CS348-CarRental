82a13ba443330033f9837082c9a7db07ac52cb69
maevetra <70355087+maevetra@users.noreply.github.com>
2022-05-01 20:56:12 -0400

    Update Account_Sp_User_call.sql

--- Account_Sp_User_call.sql
+++ Account_Sp_User_call.sql
@@ -1,9 +1,9 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `user_call`( IN user_id int)
+CREATE DEFINER=`root`@`%` PROCEDURE `user_call`( IN user_id int)
 BEGIN
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-	SELECT U.Customer_First_name, U.Customer_last_name, U.Customer_email, U.Customer_login_username,
-		U.Customer_login_password, U.Customer_Phone_Number, A.Customer_Address1, A.Customer_Address2,
-		A.Customer_City, A.Customer_State, A.Customer_zip
+	SELECT U.Customer_First_name as first_name, U.Customer_last_name as last_name, U.Customer_email as email, U.Customer_login_username as login_name,
+		U.Customer_login_password as pass, U.Customer_Phone_Number as phone_num, A.Customer_Address1 as addr1, A.Customer_Address2 as addr2,
+        A.Customer_City as city, A.Customer_State as state,  A.Customer_zip as zip
 	FROM customers U INNER JOIN customer_address A 
 	ON U.Customer_id = A.Customer_address_id
 	WHERE U.Customer_id = user_id;
